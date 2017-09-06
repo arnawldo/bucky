2c8e91661b1deeca0c98c76a57d146a67d543785
Arnold Taremwa <taremwaarnold@gmail.com>
2017-09-06 18:08:11 +0300

    added app manager and user tests

--- tests/test_app.py
+++
@@ -1,4 +0,0 @@
-def test__hello_world__succeeds(client):
-    """test hello world from basic app"""
-    r = client.get('/')
-    assert b"Hello" in r.data
--- tests/test_app_manager.py
+++ tests/test_app_manager.py
@@ -1,29 +1,51 @@
 import pytest
-from bucky.models import AppManager, User
+
+from bucky.exceptions import UserAlreadyExistsError, UserNotExistsError
+from bucky.models import AppManager
+
 
 @pytest.fixture
-def app_manager():
+def am():
     """Fixture for AppManager class
     """
     am = AppManager()
     return am
 
-@pytest.fixture
-def user1():
-    """Fixture for a unique User
-    """
-    user = User()
-    return user
 
 def test__app_manager_can_create_user__succeeds(am):
     """Make sure users can be created"""
-    am.create_user(username="uname", password="passy", email="user@gmail.com")
+    am.create_user(username="uname",
+                   password="passy",
+                   email="user@gmail.com")
     assert len(am.users) == 1
-    assert am.username == "uname"
+
 
 def test__app_manager_can_delete_user__succeeds(am):
     """Make sure users can be deleted"""
-    am.create_user(username="uname", password="passy", email="user@gmail.com")
+    am.create_user(username="uname",
+                   password="passy",
+                   email="user@gmail.com")
     assert len(am.users) == 1
     am.delete_user(username="uname")
     assert len(am.users) == 0
+
+
+def test__app_manager_create_existing_user__raises(am):
+    """Make sure app manager raises exception when creating user that
+    already exists
+    """
+    am.create_user(username="uname",
+                   password="passy",
+                   email="user@gmail.com")
+    with pytest.raises(UserAlreadyExistsError):
+        am.create_user(username="uname",
+                       password="passy",
+                       email="user@gmail.com")
+
+
+def test__app_manager_delete_non_existant_user__raises(am):
+    """Make sure app manager raises exception when creating user that
+     already exists
+     """
+    with pytest.raises(UserNotExistsError):
+        am.delete_user(username="uname")
