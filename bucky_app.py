9cfbc43e0cb45652372c30c64881281e315fd1bb
Arnold Taremwa <taremwaarnold@gmail.com>
2017-09-08 11:20:20 +0300

    added user, bucketlist, task to shell context

--- bucky_app.py
+++ bucky_app.py
@@ -2,10 +2,11 @@ import os
 from bucky import create_app
 import click
 
+from bucky.models import Task, BucketList, User, AppManager
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, AppManager=AppManager, User=User, BucketList=BucketList, Task=Task)
